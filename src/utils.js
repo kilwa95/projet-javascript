929a6c165e439bf1e22285e992513692a48f32e2
khaled <lga.sig.khaled@gmail.com>
2021-01-20 18:29:41 +0100

    utlis

---
+++ src/utils.js
@@ -0,0 +1,22 @@
+export function getValue(obj, keys) {
+  let value = obj;
+  for (keys of keys) {
+    value = value[keys];
+  }
+  return value;
+}
+
+export function interpolate(props) {
+  let template = this;
+  while (template.indexOf("{{") > 0) {
+    const start = template.indexOf("{{");
+    const end = template.indexOf("}}");
+    const keysString = template.substring(start + 2, end);
+    const keys = keysString.trim().split(".");
+    const toReplace = template.substring(start, end + 2);
+    const value = getValue(props, keys);
+    template = template.replace(toReplace, value);
+  }
+  console.log("interpolate", { props, template });
+  return template;
+}
