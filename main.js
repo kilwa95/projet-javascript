7657b594de7420921ea5bce74f323312bdab6c78
khaled <lga.sig.khaled@gmail.com>
2021-01-22 11:54:37 +0100

    Compoenet-react

--- main.js
+++ main.js
@@ -1,6 +1,2 @@
-import { ReactDOM } from './src/React';
-import HelloWorld from './src/HelloWorld';
-
-console.log(HelloWorld);
-
-ReactDOM.render(React.createElement(HelloWorld, null, null), document.getElementById('root'));
+const root = document.getElementById('root');
+console.log(root);
--- src/Component.js
+++ src/Component.js
@@ -1,15 +1,36 @@
+import _ from 'lodash';
+
 class Component {
-	state = {};
 	constructor(props) {
 		this.props = props;
+		this.state = {};
+		this.prevState = {};
+		this.prevProps = {};
+		this.prevRender = undefined;
 	}
+
+	setState(newSate) {
+		this.prevState = this.state;
+		this.state = newSate;
+	}
+
 	display(props) {
-		//...
-		return this.render();
+		this.prevProps = this.props;
+		this.props = props;
+		const shouldUpdate = this.shouldUpdate();
+		if (shouldUpdate || !this.prevRender) {
+			this.prevRender = this.render();
+		}
+		return this.prevRender;
 	}
+
+	shouldUpdate() {
+		const equalProps = _.isEqual(this.prevProps, this.props);
+		return !equalProps;
+	}
+
 	render() {
 		return;
 	}
 }
-
 export default Component;
