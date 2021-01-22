2367d4305be41d9315d5bde1f2bab79986e780e7
khaled <lga.sig.khaled@gmail.com>
2021-01-22 11:57:01 +0100

    class-react

--- src/React.js
+++ src/React.js
@@ -1,24 +1,60 @@
+import { getValue } from './String';
+
+String.prototype.interpolate = function(props) {
+	let template = this;
+	while (template.indexOf('{{') > 0) {
+		const start = template.indexOf('{{');
+		const end = template.indexOf('}}');
+		const keysString = template.substring(start + 2, end);
+		const keys = keysString.trim().split('.');
+		const toReplace = template.substring(start, end + 2);
+		const value = getValue(props, keys);
+		template = template.replace(toReplace, value);
+	}
+	//console.log("interpolate", { props, template });
+	return template;
+};
+
 export let ReactDOM = {
 	render(rElement, hElement) {
-		hElement.appendChild(rElement);
+		if (rElement) {
+			// il delcalncher le reRender ( state) Recuper  le valeur de setSate
+			hElement.appendChild(rElement);
+		}
 	}
 };
 
 export let React = {
 	createElement(tagOrComponent, props, children) {
 		let element;
-		if (tagOrComponent === 'div') {
+		let text;
+
+		if (typeof tagOrComponent === 'string') {
 			element = document.createElement(tagOrComponent);
+
 			for (let attribute in props) {
-				element.setAttribute(attribute, props[attribute]);
+				if (attribute.startsWith('on')) {
+					const event = attribute.substring(2).toLowerCase();
+					element.addEventListener(event, props[attribute]);
+				} else {
+					element.setAttribute(attribute, props[attribute]);
+				}
+
+				// si l'attrubut demare par on je recupere l'event: addEventListerner
 			}
-			for (let subElement of children) {
-				if (typeof subElement === 'string')
-					subElement = document.createTextNode(subElement /**.interpolate(props) */);
-				element.appendChild(subElement);
+
+			if (typeof children === 'string') {
+				text = document.createTextNode(children.interpolate(props));
+				element.appendChild(text);
+			} else {
+				for (let subElement of children) {
+					if (typeof subElement === 'string')
+						subElement = document.createTextNode(subElement /**.interpolate(props) */);
+					element.appendChild(subElement);
+				}
 			}
 		} else {
-			/** component **/ if (!type_check(props, tagOrComponent.propTypes)) throw new TypeError();
+			//if (!type_check(props, tagOrComponent.propTypes)) throw new TypeError();
 			return tagOrComponent.display(props);
 		}
 
