c356718251b789a4787778a5493fc8fc2f700541
khaled <lga.sig.khaled@gmail.com>
2021-01-25 20:43:16 +0100

    main-class

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
-import { ReactDOM, React } from './React.js';
-import App from './App.js';
+import { ReactDOM, React } from './React/React.js';
+import App from './Components/App.js';
+const log = console.log;
 
-//const helloWorld = new HelloWorld();
+let event = new Event('updateReactDom');
+let root = document.getElementById('root');
 
-ReactDOM.render(React.createElement(new App(), null), document.getElementById('root'));
+root.addEventListener('updateReactDom', (e) => {
+	root.innerHTML = '';
+	ReactDOM.render(React.createElement({ id: 'root' }, App, null, null), root);
+});
+root.dispatchEvent(event);
