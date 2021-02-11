b0071972bd083fb63754abef6f7e1c0edb11c6b5
Sassan <57224674+sassank@users.noreply.github.com>
2021-02-11 23:28:27 +0100

    Add files via upload

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -11,6 +11,7 @@ class Home extends Component {
 	}
 	async getFromLocalStorage() {
 		let products = JSON.parse(window.localStorage.getItem('products'));
+		let cars = JSON.parse(window.localStorage.getItem('cars'));
 		let comments = JSON.parse(window.localStorage.getItem('comments'));
 
 		this.setState({ products: products, comments: comments, isfetching: true });
@@ -18,15 +19,18 @@ class Home extends Component {
 	async willComponentMount() {
 		
 		try {
-			let [responseProducts, responseComments] = await Promise.all([
+			let [responseProducts, responseCars, responseComments] = await Promise.all([
 				fetch("https://fakestoreapi.com/products"),
+				fetch("https://private-anon-d4f6e4b1f0-carsapi1.apiary-mock.com/manufacturers"),
 				fetch("https://jsonplaceholder.typicode.com/comments")
 			]);
 			
 			const resultProducts = await responseProducts.json();
+			const resultCars = await responseCars.json();
 			const resultComments = await responseComments.json();
 
 			window.localStorage.setItem('products', JSON.stringify(resultProducts));
+			window.localStorage.setItem('cars', JSON.stringify(resultCars));
 			window.localStorage.setItem('comments', JSON.stringify(resultComments));
 			this.getFromLocalStorage();
 		}
@@ -45,7 +49,7 @@ class Home extends Component {
 			this,
 			'div',
 			null,
-			React.createElement(this, Menu, { products: this.state.products, comments: this.state.comments, isfetching: this.state.isfetching }, null),
+			React.createElement(this, Menu, { products: this.state.products, cars: this.state.cars, comments: this.state.comments, isfetching: this.state.isfetching }, null),
 			React.createElement(
 				this,
 				Article,
